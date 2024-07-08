7a36a7e79067e09e1ffca4f0a8a1387f01abb760
gianluigi7 <gianluigiferraioli7@gmail.com>
2024-07-08 17:02:23 +0200

    template

---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+import './bootstrap';
+
+//file scss default
+import '~resources/scss/app.scss';
+
+//path per gestione immagini con vite
+import.meta.glob(["../img/**"]);
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Template</title>
+    @vite('resources/js/app.js')
+</head>
+<body>
+    <h1>template Laravel</h1>
+    <h2>prova</h2>
+
+<!-- ESEMPIO IMG
+<img src="{{ Vite::asset('resources/img/logo.png') }}" alt=""> -->
+</body>
+</html>
